87b0d617eb2b86a399b499f2ca44cd95822f2827
Jesse Carvalho Amorim <jessecaramorim@gmail.com>
2022-12-27 13:10:18 -0400

    Exemplos de FOR

---
+++ controle -  fluxo/src/ExemploFor.java
@@ -0,0 +1,9 @@
+public class ExemploFor {
+    public static void main(String[] args) {
+        
+        for (Integer carneirinhos = 1; carneirinhos <= 20; carneirinhos ++){
+            System.out.println("Contando carneirinhos: " + carneirinhos);
+        }
+    System.out.println("Pietro dormiu!"); 
+    }
+}
